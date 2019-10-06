a45d3c5d844df5844a62c24d4d2eb2678fd16253
José Luis <jose02xuma@gmail.com>
2019-10-06 00:12:57 -0500

    desarrollando la ruta guardar y configurando las variables de entorno

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const sha1 = require('sha1');
 const cors = require('cors');
+const env = require('node-env-file');
+env(path.join(__dirname, '../.env'));
 
 app.use(bodyParser.json());
 app.use(cors());
 const router = express.Router();
 
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'prueba'
+    host : process.env.HOST,
+    user : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE
 });
 
 db.connect();
@@ -39,18 +41,15 @@ router.use(function(req, res){
 });
 
 app.post("/api/buscar/", function(req, res, next){
-    let token = '2f8f64ebec31084a75213053ec72070d673b74e8';
-    let numero = req.body.numero;
-    let tipo = req.body.tipo;
     let sql = "SELECT id FROM tokens WHERE token = ?";
-    console.log(tipo);
+    let sql2 = "";
 
-    db.query(sql, [token], (err, result)=>{
+    db.query(sql, [process.env.TOKEN], (err, result)=>{
         if(err) throw err;
         if(result == ''){
             res.json([{"id": "", "nombre":"", "descripcion":"", "matchs":"", "subida":"", "modificacion":"", "tipo":""}]);
         }else{
-            if(numero == 1){
+            if(req.body.numero == 1){
                 sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM favoritos WHERE nombre LIKE ? ORDER BY id DESC";
                 db.query(sql2, ["%" + req.body.palabra + "%"], (err, result)=>{
                     if(err) throw err;
@@ -60,9 +59,20 @@ app.post("/api/buscar/", function(req, res, next){
                         res.json(result);
                     }
                 });
-            }else if(numero == 2){
-                sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY id ASC";
-                db.query(sql2, [tipo, "%" + req.body.palabra + "%"], (err, result)=>{
+            }else if(req.body.numero == 2){
+                if(req.body.orden == 'id'){
+                    sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY id DESC";
+                }else if(req.body.orden == 'snombre'){
+                    sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY nombre DESC";
+                }else if(req.body.orden == 'smatch'){
+                    sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY matchs DESC";
+                }else if(req.body.orden == 'ssubida'){
+                    sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY subida DESC";
+                }else if(req.body.orden == 'smodificacion'){
+                    sql2 = "SELECT id, nombre, descripcion, matchs, subida, modificacion, tipo FROM segmentos WHERE tipo = ? AND nombre LIKE ? ORDER BY modificacion DESC";
+                }
+
+                db.query(sql2, [req.body.tipo, "%" + req.body.palabra + "%"], (err, result)=>{
                     if(err) throw err;
                     if(result == ''){
                         res.json([{"id": "", "nombre":"", "descripcion":"", "matchs":"", "subida":"", "modificacion":"", "tipo":""}]);
@@ -76,6 +86,70 @@ app.post("/api/buscar/", function(req, res, next){
         }
     });
 });
+
+app.post("/api/guardar/", function(req, res, next){
+    if(req.body.snombre != '' && req.body.snombre != ' '){
+        if(req.body.sdescripcion != '' && req.body.sdescripcion != ' '){
+            if(req.body.smatch != '' && req.body.smatch != ' '){
+                if(entero(req.body.smatch)){
+                    if(req.body.stipo != '' && req.body.stipo != ' '){
+                        hoy = new Date();
+                        dd = hoy.getDate();
+                        mm = hoy.getMonth()+1;
+                        yyyy = hoy.getFullYear();
+                        hh = hoy.getHours();
+                        mmm = hoy.getMinutes();
+                        ss = hoy.getSeconds();
+
+                        let dia = yyyy + '-' + fecha(mm) + '-' + fecha(dd);
+                        let hora = hh + ':' + mmm + ':' + ss;
+
+                        let sql = "SELECT id FROM tokens WHERE token = ?";
+
+                        db.query(sql, [process.env.TOKEN], (err, result)=>{
+                            if(err) throw err;
+                            if(result == ''){
+                                res.json([{"mensaje": "¡Lo sentimos, intentalo nuevamente!", "estado": false}]);
+                            }else{
+                                sql = 'INSERT INTO segmentos SET ?';
+                                db.query(sql, { nombre: req.body.snombre, descripcion: req.body.sdescripcion, matchs: req.body.smatch, subida: dia, modificacion: dia, tipo: req.body.stipo}, (err, result)=>{
+                                    if(err) throw err;
+                                    if(result.insertId){
+                                        res.json([{"mensaje": "guardado exitosamante", "estado": true}]);
+                                    }
+                                });
+                            }
+                        });
+                    }else{
+                        res.json([{"mensaje": "elige un tipo", "estado": false}]);
+                    }
+                }else{
+                    res.json([{"mensaje": "el match debe ser número", "estado": false}]);
+                }
+            }else{
+                res.json([{"mensaje": "ingresa un match", "estado": false}]);
+            }
+        }else{
+            res.json([{"mensaje": "ingresa una descripción", "estado": false}]);
+        }
+    }else{
+        res.json([{"mensaje": "ingresa un nombre", "estado": false}]);
+    }
+});
+
+function fecha(params){
+    if (params < 10) {
+        params = '0' + params;
+    }
+    return params;
+}
+
+function entero(x) {
+  if (isNaN(x)) {
+    return false;
+  }
+  return true;
+}
 
 app.set('port', process.env.PORT || 3030);
 app.set('json spaces', 2);
